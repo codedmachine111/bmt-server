ace09a483f50ae61f46bc76491c0c8add0a8e936
codedmachine111 <charlesbabbage1709@gmail.com>
2023-06-25 10:29:00 +0530

    delete booking

--- routes/Booking.js
+++ routes/Booking.js
@@ -9,54 +9,67 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 router.post("/add", validateToken, async (req, res) => {
-    const {service, date, expertName, userId} = req.body;
-    const booking = await prisma.serviceTicket.create({
-        data: {
-            service: service,
-            date: date,
-            status: 'pending',    
-            userId: userId,
-            expertName: expertName,        
-        }
-    })
-    if(!booking){
-        res.json({error: 'Error creating booking'});
-    }else{
-        res.json({message: 'Booking created'});
-    }
-})
+  const { service, date, expertName, userId } = req.body;
+  const booking = await prisma.serviceTicket.create({
+    data: {
+      service: service,
+      date: date,
+      status: "pending",
+      userId: userId,
+      expertName: expertName,
+    },
+  });
+  if (!booking) {
+    res.json({ error: "Error creating booking" });
+  } else {
+    res.json({ message: "Booking created" });
+  }
+});
 
 router.get("/all/:id", validateToken, async (req, res) => {
-    const { id } = req.params;
-    const bookings = await prisma.serviceTicket.findMany({
-      where: {
-        userId: parseInt(id),
-      },
-    });
-  
-    if (!bookings) {
-      res.json({ error: "No bookings found" });
-    } else {
-      res.json(bookings);
-    }
+  const { id } = req.params;
+  const bookings = await prisma.serviceTicket.findMany({
+    where: {
+      userId: parseInt(id),
+    },
   });
-  
+
+  if (!bookings) {
+    res.json({ error: "No bookings found" });
+  } else {
+    res.json(bookings);
+  }
+});
 
 router.put("/update", validateToken, async (req, res) => {
-    const {id, status} = req.body;
-    const booking = await prisma.serviceTicket.update({
-        where: {
-            id: id
-        },
-        data: {
-            status: status
-        }
-    })
-    if(!booking){
-        res.json({error: 'Error updating booking'});
-    }else{
-        res.json({message: 'Booking updated'});
-    }
-})
+  const { id, status } = req.body;
+  const booking = await prisma.serviceTicket.update({
+    where: {
+      id: id,
+    },
+    data: {
+      status: status,
+    },
+  });
+  if (!booking) {
+    res.json({ error: "Error updating booking" });
+  } else {
+    res.json({ message: "Booking updated" });
+  }
+});
+
+router.delete("/delete/:id", validateToken, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedBooking = await prisma.serviceTicket.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    res.json({ message: "Booking deleted" });
+  } catch (error) {
+    res.status(500).json({ error: "Error deleting booking" });
+  }
+});
 
 module.exports = router;
